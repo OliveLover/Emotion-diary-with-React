8c93683d7e0d76a5a74d9c9326d679e21418cccf
tiktokyi <tiktokyi@gmail.com>
2024-06-16 16:59:56 +0900

    Feat : New 페이지의 헤더 섹션 구현

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,5 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../component/Button";
+import Header from "../component/Header";
+
 const New = () => {
-  return <div>New 페이지입니다.</div>;
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
+  return (
+    <div>
+      <Header
+        title={"새 일기 쓰기"}
+        leftChild={<Button text={"< 뒤로 가기"} onClick={goBack} />}
+      />
+    </div>
+  );
 };
 
 export default New;
