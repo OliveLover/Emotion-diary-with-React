a8118e6803eb0ea624cc3fc71704cdf27fcf2072
tiktokyi <tiktokyi@gmail.com>
2024-06-16 12:18:47 +0900

    Feat : 버튼 컴포넌트의 기본 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,30 @@
-import { useSearchParams } from "react-router-dom";
+import Button from "../component/Button";
 
 const Home = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  console.log(searchParams.get("sort"));
-  return <div>Home 페이지입니다.</div>;
+  return (
+    <div>
+      <Button
+        text={"기본 버튼"}
+        onClick={() => {
+          alert("default button");
+        }}
+      />
+      <Button
+        type="positive"
+        text={"긍정 버튼"}
+        onClick={() => {
+          alert("positive button");
+        }}
+      />
+      <Button
+        type="negative"
+        text={"부정 버튼"}
+        onClick={() => {
+          alert("negative button");
+        }}
+      />
+    </div>
+  );
 };
 
 export default Home;
